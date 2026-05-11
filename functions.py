0a67e83cdb0a84325cfce547e2ea66abbed54181
raghini bhaskar <raghinibhaskar1@gmail.com>
2026-05-11 18:16:05 +0530

    completed functions pratice

---
+++ functions.py
@@ -0,0 +1,46 @@
+def greet():
+    print("Hello World")
+
+greet()
+
+# functions with parameters 
+def greet(name):
+    print("Hello", name)
+
+greet("Raghini")
+
+def add(a, b):
+    return a + b
+
+result = add(5, 3)
+print(result)
+
+def greet(name="Guest"):
+    print("Hello", name)
+
+greet()
+greet("Raghini")
+
+def hello():
+    print("Hello")
+
+def world():
+    hello()
+    print("World")
+
+world()
+
+def count():
+    for i in range(5):
+        print(i)
+
+count()
+
+def check_even(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+for i in range(1, 6):
+    print(i, check_even(i))
