04245f381287f1cd80255b096e7051839659deff
raghini bhaskar <raghinibhaskar1@gmail.com>
2026-05-11 17:52:01 +0530

    completed loops pratice

---
+++ loops.py
@@ -0,0 +1,57 @@
+# for loop
+for x in range(1, 10):
+    print(x)
+
+fruits = ["apple", "banana", "cherry"]
+
+for x in fruits:
+  print(x)
+
+for i in range(9):
+  if i > 3:
+    break
+  print(i)
+
+i = 1
+while i < 9:
+  print(i)
+  if i == 3:
+    break
+  i += 1   
+
+for i in range(9):
+  if i == 3:
+    continue
+  print(i)  
+
+#for loop
+for i in range(5):
+  print(i)
+
+#while loop
+x = 0
+while x < 5:
+  print(x)
+  x += 1
+
+# break 
+for i in range(5):
+  if i == 3:
+    break
+  print(i)
+
+   # example 
+   # FOR loop example
+print("For Loop:")
+
+for i in range(1, 6):
+    print(i)
+
+# WHILE loop example
+print("While Loop:")
+
+x = 1
+while x <= 5:
+    print(x)
+    x += 1    
+  
