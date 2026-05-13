6ecd4a97bf7375099a8301716a7655f7b20471c5
raghini bhaskar <raghinibhaskar1@gmail.com>
2026-05-13 15:29:13 +0530

    compeleted lambda pratice

---
+++ lamda.py
@@ -0,0 +1,19 @@
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+
+print(mydoubler(11))
+
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+mytripler = myfunc(3)
+
+print(mydoubler(11))
+print(mytripler(11))
+
+students = [("Emil", 25), ("Tobias", 22), ("Linus", 28)]
+sorted_students = sorted(students, key=lambda x: x[1])
+print(sorted_students)
