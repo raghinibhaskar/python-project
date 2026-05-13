bc06cf794c72f77317f39c5da1014d6f1a7c7080
raghini bhaskar <raghinibhaskar1@gmail.com>
2026-05-13 17:02:52 +0530

    compeleted none pratice

---
+++ none.py
@@ -0,0 +1,24 @@
+x = None
+print(x)
+
+x = None
+print(type(x))
+
+result = None
+if result is None:
+  print("No result yet")
+else:
+  print("Result is ready")
+
+result = None
+if result is not None:
+  print("Result is ready")
+else:
+  print("No result yet")
+
+  def myfunc():
+  x = 5
+
+x = myfunc()
+print(x)
+
