2e1d50a0bbea7db4094750dde1dd3829d4c53871
raghini bhaskar <raghinibhaskar1@gmail.com>
2026-05-13 15:20:03 +0530

    compeleted scope practiced

---
+++ scope.py
@@ -0,0 +1,64 @@
+def myfunc():
+  x = 300
+  print(x)
+
+myfunc()
+
+def myfunc():
+  x = 300
+  def myinnerfunc():
+    print(x)
+  myinnerfunc()
+
+myfunc()
+
+x = 300
+
+def myfunc():
+  print(x)
+
+myfunc()
+
+print(x)
+
+#naming variables
+x = 300
+
+def myfunc():
+  x = 200
+  print(x)
+
+myfunc()
+
+print(x)
+
+def myfunc():
+  global x
+  x = 300
+
+myfunc()
+
+print(x)
+
+def myfunc1():
+  x = "Jane"
+  def myfunc2():
+    nonlocal x
+    x = "hello"
+  myfunc2()
+  return x
+
+print(myfunc1())
+
+x = "global"
+
+def outer():
+  x = "enclosing"
+  def inner():
+    x = "local"
+    print("Inner:", x)
+  inner()
+  print("Outer:", x)
+
+outer()
+print("Global:", x)
