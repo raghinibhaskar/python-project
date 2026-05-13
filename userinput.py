22c522b496e6f5e2bea5d41d3c858e69a5f83162
raghini bhaskar <raghinibhaskar1@gmail.com>
2026-05-13 17:07:51 +0530

    compeleted userinput

---
+++ userinput.py
@@ -0,0 +1,28 @@
+print("Enter your name:")
+name = input()
+print(f"Hello {name}")
+
+name = input("Enter your name:")
+print(f"Hello {name}")
+fav1 = input("What is your favorite animal:")
+fav2 = input("What is your favorite color:")
+fav3 = input("What is your favorite number:")
+print(f"Do you want a {fav2} {fav1} with {fav3} legs?")
+
+x = input("Enter a number:")
+
+#find the square root of the number:
+y = math.sqrt(float(x))
+
+print(f"The square root of {x} is {y}")
+
+y = True
+while y == True:
+  x = input("Enter a number:")
+  try:
+    x = float(x);
+    y = False
+  except:
+    print("Wrong input, please try again.")
+
+print("Thank you!")
